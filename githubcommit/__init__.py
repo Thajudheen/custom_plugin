3a41bca838da7a385b47378eaee40f9f6412e646
Thaj <Thajudheen.rajak@ibm.com>
2019-10-10 07:00:13 +0000

    renamed dir

---
+++ githubcommit/__init__.py
@@ -0,0 +1,17 @@
+from custom_plugin.handlers import setup_handlers
+# Jupyter Extension points
+def _jupyter_server_extension_paths():
+    return [{
+        'module': 'custom_plugin',
+    }]
+
+def _jupyter_nbextension_paths():
+    return [{
+        "section": "notebook",
+        "dest": "custom_plugin",
+        "src": "static",
+        "require": "custom_plugin/main"
+    }]
+
+ def load_jupyter_server_extension(nbapp):
+     setup_handlers(nbapp.web_app)
